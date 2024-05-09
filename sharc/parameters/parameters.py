baac7ac1a6f7075c217bf5ad43f4a2535ed71930
Bruno Faria <brunohcfaria@gmail.com>
2024-05-09 11:36:22 -0300

    Added parameters for ARNS system
    Signed-off-by: Bruno Faria <brunohcfaria@gmail.com>

--- sharc/parameters/parameters.py
+++ sharc/parameters/parameters.py
@@ -21,6 +21,7 @@ from sharc.parameters.parameters_fss_es import ParametersFssEs
 from sharc.parameters.parameters_haps import ParametersHaps
 from sharc.parameters.parameters_rns import ParametersRns
 from sharc.parameters.parameters_ras import ParametersRas
+from sharc.parameters.parameters_arns import ParametersArns
 
 
 class Parameters(object):
@@ -43,6 +44,7 @@ class Parameters(object):
         self.haps = ParametersHaps()
         self.rns = ParametersRns()
         self.ras = ParametersRas()
+        self.arns = ParametersArns()
 
 
     def set_file_name(self, file_name: str):
@@ -126,6 +128,12 @@ class Parameters(object):
         # EESS passive
         #######################################################################
         self.eess_passive.load_parameters_from_file(self.file_name)
+
+        #######################################################################
+        # ARNS
+        #######################################################################
+        self.arns.load_parameters_from_file(self.file_name)
+
 
 if __name__ == "__main__":
     from pprint import pprint
